158adf452ade71a64d47557922ce07ead4af2ff2
Jeff Klenzing <jklenzing@users.noreply.github.com>
2022-05-18 17:52:28 -0400

    ENH: loop over all inst_ids

--- scripts/netcdf_gen.py
+++ scripts/netcdf_gen.py
@@ -6,25 +6,26 @@ from ops_reach.instruments import aero_reach
 import pysat
 
 # Vehicle ID of REACH instrument
-inst_id = '101'
+inst_ids = ['101', '105']
 
 # Figure out directory for final files
 path = os.path.join(pysat.params['data_dirs'][0], 'aero', 'reach', 'l1c')
 if not os.path.isdir(path):
     os.mkdir(path)
 
-# Generate main reach instrument
-reach = pysat.Instrument(inst_module=aero_reach, tag='l1b', inst_id=inst_id)
+for inst_id in inst_ids:
+    # Generate main reach instrument
+    reach = pysat.Instrument(inst_module=aero_reach, tag='l1b', inst_id=inst_id)
 
-for date in reach.files.files.index:
-    # Generate outfile name
-    fname = reach.files.files[date]
-    fname = fname.replace('l1b', 'l1c')
-    fname = fname.replace('csv', 'nc')
-    outfile = os.path.join(path, fname)
+    for date in reach.files.files.index:
+        # Generate outfile name
+        fname = reach.files.files[date]
+        fname = fname.replace('l1b', 'l1c')
+        fname = fname.replace('csv', 'nc')
+        outfile = os.path.join(path, fname)
 
-    # Get data
-    reach.load(date=date)
+        # Get data
+        reach.load(date=date)
 
-    # Ouput data
-    pysat.utils.io.inst_to_netcdf(reach, outfile)
+        # Ouput data
+        pysat.utils.io.inst_to_netcdf(reach, outfile)
