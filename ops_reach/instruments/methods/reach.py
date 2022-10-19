581ff4aea087ed8ca478dd5cc4cedfe0be898596
Jeff Klenzing <jklenzing@users.noreply.github.com>
2022-10-19 15:06:13 -0400

    STY: cleanup

--- ops_reach/instruments/methods/reach.py
+++ ops_reach/instruments/methods/reach.py
@@ -70,7 +70,7 @@ def scrub_l1b(data):
                                 'HK 15V Monitor': 'hk_15v_monitor',
                                 'HK 5V Monitor': 'hk_5v_monitor',
                                 'HK 3.3V Monitor': 'hk_3_3v_monitor',
-                                'Lm RE': 'lm', 'Inv_Lat deg': 'inverse_lat',
+                                'Lm RE': 'lm', 'Inv_Lat deg': 'inv_lat',
                                 'Blocal nT': 'blocal', 'Bmin nT': 'bmin',
                                 'MLT hr': 'mlt', 'K sqrt(G)RE': 'k_sqrt',
                                 'hmin km': 'hmin', 'Alpha deg': 'alpha',
@@ -176,7 +176,8 @@ def generate_metadata(header_data):
     # Create required metadata values
     meta = pysat.Meta(header_data=header_data)
 
-    meta['mjd'] = {meta.labels.units: 'Julian Day',
+    meta['mjd'] = {meta.labels.name: 'Modified Julian Day',
+                   meta.labels.units: 'Julian Day',
                    meta.labels.min_val: 0.0,
                    meta.labels.max_val: np.nan}
     meta['year'] = {meta.labels.units: 'years',
@@ -197,19 +198,24 @@ def generate_metadata(header_data):
     meta['seconds'] = {meta.labels.units: 'seconds',
                        meta.labels.min_val: 0.0,
                        meta.labels.max_val: 60.0}
-    meta['doy'] = {meta.labels.units: 'days',
+    meta['doy'] = {meta.labels.name: 'Day of Year',
+                   meta.labels.units: 'days',
                    meta.labels.min_val: 0,
                    meta.labels.max_val: 366}
-    meta['vid'] = {meta.labels.units: 'unitless',
+    meta['vid'] = {meta.labels.name: 'REACH Vehicle Identifier',
+                   meta.labels.units: 'unitless',
                    meta.labels.min_val: 0.0,
                    meta.labels.max_val: np.nan}
-    meta['altitude'] = {meta.labels.units: 'km',
+    meta['altitude'] = {meta.labels.name: 'Geodetic Altitude',
+                        meta.labels.units: 'km',
                         meta.labels.min_val: 0.0,
                         meta.labels.max_val: np.inf}
-    meta['latitude'] = {meta.labels.units: 'degrees',
+    meta['latitude'] = {meta.labels.name: 'Geodetic Latitude',
+                        meta.labels.units: 'degrees',
                         meta.labels.min_val: -90.0,
                         meta.labels.max_val: 90.0}
-    meta['longitude'] = {meta.labels.units: 'degrees',
+    meta['longitude'] = {meta.labels.name: 'Geodetic Longitude',
+                         meta.labels.units: 'degrees',
                          meta.labels.min_val: 0.0,
                          meta.labels.max_val: 360.0}
     meta['geo_x'] = {meta.labels.units: 'RE',
@@ -230,57 +236,83 @@ def generate_metadata(header_data):
     meta['gei_z'] = {meta.labels.units: 'RE',
                      meta.labels.min_val: -2.0,
                      meta.labels.max_val: 2.0}
-    meta['dose1'] = {meta.labels.units: 'rad/s',
+    meta['dose1'] = {meta.labels.name: 'Dose rate from Dosimeter 1',
+                     meta.labels.units: 'rad/s',
                      meta.labels.min_val: 0.0,
                      meta.labels.max_val: np.inf}
-    meta['proton_flux1'] = {meta.labels.units: '#/cm^2/sr/s',
+    meta['proton_flux1'] = {meta.labels.name: 'Proton flux from bowtie',
+                            meta.labels.units: '#/cm^2/sr/s',
                             meta.labels.min_val: 0.0,
                             meta.labels.max_val: np.inf}
-    meta['electron_flux1'] = {meta.labels.units: '#/cm^2/sr/s',
+    meta['electron_flux1'] = {meta.labels.name: 'Electron flux from bowtie',
+                              meta.labels.units: '#/cm^2/sr/s',
                               meta.labels.min_val: 0.0,
                               meta.labels.max_val: np.inf}
-    meta['species1'] = {meta.labels.units: 'unitless',
-                        meta.labels.notes: '0 (not currently used), 1 – protons, 2-electrons, and 3-both / ambiguous',
-                        meta.labels.min_val: 0.0,
-                        meta.labels.max_val: np.nan}
-    meta['dose2'] = {meta.labels.units: 'rad/s',
+    meta['species1'] = {meta.labels.name: 'Most probable species',
+                        meta.labels.notes: ' '.join(('0 (not currently used),',
+                                                     '1 – protons, 2-electrons,',
+                                                     'and 3-both / ambiguous')),
+                        meta.labels.units: 'unitless',
+                        meta.labels.min_val: 0,
+                        meta.labels.max_val: 3}
+    meta['dose2'] = {meta.labels.name: 'Dose rate from Dosimeter 2',
+                     meta.labels.units: 'rad/s',
                      meta.labels.min_val: 0.0,
                      meta.labels.max_val: np.inf}
-    meta['proton_flux2'] = {meta.labels.units: '#/cm^2/sr/s',
+    meta['proton_flux2'] = {meta.labels.name: 'Proton flux from bowtie',
+                            meta.labels.units: '#/cm^2/sr/s',
                             meta.labels.min_val: 0.0,
                             meta.labels.max_val: np.inf}
-    meta['electron_flux2'] = {meta.labels.units: '#/cm^2/sr/s',
+    meta['electron_flux2'] = {meta.labels.name: 'Electron flux from bowtie',
+                              meta.labels.units: '#/cm^2/sr/s',
                               meta.labels.min_val: 0.0,
                               meta.labels.max_val: np.inf}
-    meta['species2'] = {meta.labels.units: 'unitless',
-                        meta.labels.notes: '0 (not currently used), 1 – protons, 2-electrons, and 3-both / ambiguous',
-                        meta.labels.min_val: 0.0,
-                        meta.labels.max_val: np.nan}
-    meta['hk_temperature'] = {meta.labels.units: 'degrees C',
+    meta['species2'] = {meta.labels.name: 'Most probable species',
+                        meta.labels.notes: ' '.join(('0 (not currently used),',
+                                                     '1 – protons, 2-electrons,',
+                                                     'and 3-both / ambiguous')),
+                        meta.labels.units: 'unitless',
+                        meta.labels.min_val: 0,
+                        meta.labels.max_val: 3}
+    meta['hk_temperature'] = {meta.labels.name: 'Housekeeping temperature',
+                              meta.labels.units: 'degrees C',
                               meta.labels.min_val: 0.0,
                               meta.labels.max_val: 20.0}
-    meta['hk_15v_monitor'] = {meta.labels.units: 'volts',
+    meta['hk_15v_monitor'] = {meta.labels.name: 'Housekeeping voltage 15V',
+                              meta.labels.units: 'volts',
                               meta.labels.min_val: 0.0,
-                              meta.labels.max_val: np.nan}
-    meta['hk_5v_monitor'] = {meta.labels.units: 'volts',
+                              meta.labels.max_val: np.inf}
+    meta['hk_5v_monitor'] = {meta.labels.name: 'Housekeeping voltage 5V',
+                             meta.labels.units: 'volts',
                              meta.labels.min_val: 0.0,
-                             meta.labels.max_val: np.nan}
-    meta['hk_3_3v_monitor'] = {meta.labels.units: 'volts',
+                             meta.labels.max_val: np.inf}
+    meta['hk_3_3v_monitor'] = {meta.labels.name: 'Housekeeping voltage 3.3V',
+                               meta.labels.units: 'volts',
                                meta.labels.min_val: 0.0,
-                               meta.labels.max_val: np.nan}
-    meta['lm'] = {meta.labels.units: 'RE',
+                               meta.labels.max_val: np.inf}
+    meta['lm'] = {meta.labels.name: ' '.join(('McIlwain L-shell for locally',
+                                              'mirroring particle')),
+                  meta.labels.units: 'RE',
                   meta.labels.min_val: 0.0,
                   meta.labels.max_val: 30.0}
-    meta['inverse_lat'] = {meta.labels.units: 'deg',
-                           meta.labels.min_val: 0.0,
-                           meta.labels.max_val: np.nan}
-    meta['blocal'] = {meta.labels.units: 'nT',
+    meta['inv_lat'] = {meta.labels.name: ' '.join(('Invariant Latitude for',
+                                                   'locally mirroring',
+                                                   'particle')),
+                       meta.labels.units: 'deg',
+                       meta.labels.min_val: 0.0,
+                       meta.labels.max_val: np.nan}
+    meta['blocal'] = {meta.labels.name: 'Local magnetic field at s/c',
+                      meta.labels.units: 'nT',
                       meta.labels.min_val: 0.0,
                       meta.labels.max_val: np.nan}
-    meta['bmin'] = {meta.labels.units: 'nT',
+    meta['bmin'] = {meta.labels.name: ' '.join(('Minimum magnetic field on',
+                                                'field line intersecting',
+                                                'spacecraft')),
+                    meta.labels.units: 'nT',
                     meta.labels.min_val: 0.0,
                     meta.labels.max_val: np.nan}
-    meta['mlt'] = {meta.labels.units: 'hours',
+    meta['mlt'] = {meta.labels.name: 'Magnetic local time',
+                   meta.labels.units: 'hours',
                    meta.labels.min_val: 0.0,
                    meta.labels.max_val: np.nan}
     meta['k_sqrt'] = {meta.labels.units: 'RE',
@@ -289,21 +321,42 @@ def generate_metadata(header_data):
     meta['hmin'] = {meta.labels.units: 'km',
                     meta.labels.min_val: 0.0,
                     meta.labels.max_val: np.nan}
-    meta['alpha'] = {meta.labels.units: 'deg',
+    meta['alpha'] = {meta.labels.name: 'Local pitch angle',
+                     meta.labels.units: 'deg',
                      meta.labels.min_val: 0.0,
                      meta.labels.max_val: 90.0}
-    meta['alpha_eq'] = {meta.labels.units: 'deg',
+    meta['alpha_eq'] = {meta.labels.name: 'Equatorial pitch angle',
+                        meta.labels.units: 'deg',
                         meta.labels.min_val: 0.0,
                         meta.labels.max_val: np.nan}
-    meta['region_code'] = {meta.labels.units: 'unitless',
-                           meta.labels.min_val: 0.0,
-                           meta.labels.max_val: np.nan}
+    meta['region_code'] = {
+        meta.labels.notes: '; '.join(('-4: Southern Polar Cap',
+                                      '-3: Outer Zone Untrapped',
+                                      '-2: Slot Untrapped',
+                                      '-1: Inner Zone Untrapped',
+                                      '0: Unknown',
+                                      '1: Inner Zone Trapped',
+                                      '2: Slot Trapped',
+                                      '3: Outer Zone Trapped',
+                                      '4: Northern Polar Cap')),
+        meta.labels.units: 'unitless',
+        meta.labels.min_val: -4,
+        meta.labels.max_val: 4}
     meta['orbit_status'] = {meta.labels.units: 'unitless',
-                            meta.lables.notes: '1=Northbound, 2=Southbound',
+                            meta.labels.notes: '1=Northbound; 2=Southbound',
                             meta.labels.min_val: 0.0,
                             meta.labels.max_val: np.nan}
-    meta['flag'] = {meta.labels.units: 'unitless',
-                    meta.labels.min_val: 0.0,
-                    meta.labels.max_val: np.nan}
+    meta['flag'] = {
+        meta.labels.notes: '; '.join((
+            '-1: no data',
+            '0: No known problems',
+            '1: Test Mode',
+            '2: Possible temperature-related self-counting in Dosimeter A.',
+            '4: Possible temperature-related self-counting in Dosimeter B.',
+            '8: Duplicate packets detected.',
+            '16: Unknown issue with VID 163/Dosimeter B')),
+        meta.labels.units: 'unitless',
+        meta.labels.min_val: -1,
+        meta.labels.max_val: 16}
 
     return meta
