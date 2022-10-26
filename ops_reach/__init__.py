b0860ffd6b45f337476d70dc438487b235d141c6
Jeff Klenzing <jklenzing@users.noreply.github.com>
2022-10-26 12:55:14 -0400

    STY: one source of version

--- ops_reach/__init__.py
+++ ops_reach/__init__.py
@@ -6,12 +6,10 @@ REACH instruments for conversion of level 1b data to level 1c.
 """
 
 import os
+import pkg_resources
+
 from ops_reach import instruments  # noqa F401
 
 # set version
 __here__ = os.path.abspath(os.path.dirname(__file__))
-version_filename = os.path.join(__here__, 'version.txt')
-with open(version_filename, 'r') as version_file:
-    __version__ = version_file.read().strip()
-
-del version_filename, version_file
+__version__ = pkg_resources.get_distribution('ops_reach').version
